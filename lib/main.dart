8387f20e8b9a081f1d38581aa22b4003d4ff5f32
Joshua Pirwoth <joshuapirwoth06@gmail.com>
2024-05-30 10:55:47 +0300

    Crisis Hotlines as the home widget, with Appbar and title

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/pages/crisis_hotlines.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello, World!'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false, // Remove the debug banner
+      home: CrisisHotlines(), // Crisis Hotlines Widget
     );
   }
 }
---
+++ lib/pages/crisis_hotlines.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CrisisHotlines extends StatefulWidget {
+  const CrisisHotlines({super.key});
+
+  @override
+  State<CrisisHotlines> createState() => _CrisisHotlinesState();
+}
+
+class _CrisisHotlinesState extends State<CrisisHotlines> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // const keyword removed
+      backgroundColor: Color(0xFFFCFEFF), // Bg color hexcode is #FCFEFF
+      appBar: AppBar(
+        backgroundColor: Color(0xFF647AFF),
+        title: Text(
+          'Crisis Hotlines',
+          style: TextStyle(color: Color(0xFFFCFEFF)),
+        ),
+      ),
+    );
+  }
+}
