5b48e31bedf61c349de78e747792379c7028ebe3
Joshua Pirwoth <joshuapirwoth06@gmail.com>
2024-05-31 16:29:06 +0300

    Made the tile clickable with logic for navigation to the GroupEvents widget

--- lib/util/tab_bar_tile.dart
+++ lib/util/tab_bar_tile.dart
@@ -29,78 +29,85 @@ class TabBarTile extends StatelessWidget {
           ),
         ),
         padding: EdgeInsets.fromLTRB(0, 0, 0, 12.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // Expanded to take available space
-            Expanded(
-              child: Row(
-                children: [
-                  // Circle avatar with groupName initial
-                  CircleAvatar(
-                    radius: 24,
-                    backgroundColor: avatarColor,
-                    child: Text(
-                      groupName[0].toUpperCase(),
-                      style: TextStyle(
-                          fontSize: 28.0,
-                          color: Color(0xFFFCFEFF),
-                          fontWeight: FontWeight.w200),
+        child: GestureDetector(
+          onTap: () {
+            // onTap logic here for navigating to support group details
+            Navigator.pushNamed(context, '/groupEvents');
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // Expanded to take available space
+              Expanded(
+                child: Row(
+                  children: [
+                    // Circle avatar with groupName initial
+                    CircleAvatar(
+                      radius: 24,
+                      backgroundColor: avatarColor,
+                      child: Text(
+                        groupName[0].toUpperCase(),
+                        style: TextStyle(
+                            fontSize: 28.0,
+                            color: Color(0xFFFCFEFF),
+                            fontWeight: FontWeight.w200),
+                      ),
                     ),
-                  ),
 
-                  // SizedBox to separate avatar and column
-                  SizedBox(width: 16.0),
+                    // SizedBox to separate avatar and column
+                    SizedBox(width: 16.0),
 
-                  // Flexible to constrain column
-                  Flexible(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          groupName,
-                          overflow: TextOverflow
-                              .ellipsis, // make long text end with '...'
-                          softWrap: false,
-                          style: TextStyle(
-                              fontSize: 16.0, color: Color(0xFF32313A)),
-                        ),
-                        Text(
-                          createdBy,
-                          overflow: TextOverflow
-                              .ellipsis, // make long text end with '...'
-                          softWrap: false,
-                          style: TextStyle(
-                              fontSize: 12.0, color: Color(0xFF666666)),
-                        ),
-                      ],
+                    // Flexible to constrain column
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            groupName,
+                            overflow: TextOverflow
+                                .ellipsis, // make long text end with '...'
+                            softWrap: false,
+                            style: TextStyle(
+                                fontSize: 16.0, color: Color(0xFF32313A)),
+                          ),
+                          Text(
+                            createdBy,
+                            overflow: TextOverflow
+                                .ellipsis, // make long text end with '...'
+                            softWrap: false,
+                            style: TextStyle(
+                                fontSize: 12.0, color: Color(0xFF666666)),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-
-            SizedBox(width: 8.0),
-
-            // Button
-            SizedBox(
-              width: 64,
-              child: AspectRatio(
-                aspectRatio: 2,
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Join',
-                    style: TextStyle(color: Color(0xFFFCFEFF), fontSize: 14.0),
-                  ),
-                  style: TextButton.styleFrom(
-                      backgroundColor: Color(0xFF58D68D),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6))),
+                  ],
                 ),
               ),
-            ),
-          ],
+
+              SizedBox(width: 8.0),
+
+              // Button
+              SizedBox(
+                width: 64,
+                child: AspectRatio(
+                  aspectRatio: 2,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Join',
+                      style:
+                          TextStyle(color: Color(0xFFFCFEFF), fontSize: 14.0),
+                    ),
+                    style: TextButton.styleFrom(
+                        backgroundColor: Color(0xFF58D68D),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6))),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
