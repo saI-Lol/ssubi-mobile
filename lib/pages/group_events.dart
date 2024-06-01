fea5f0931b7b0341342b757fab5ced4a3c6d5402
Joshua Pirwoth <joshuapirwoth06@gmail.com>
2024-06-01 15:58:07 +0300

    Updated a sample list of event data

--- lib/pages/group_events.dart
+++ lib/pages/group_events.dart
@@ -12,6 +12,28 @@ class GroupEvents extends StatefulWidget {
 }
 
 class _GroupEventsState extends State<GroupEvents> {
+  // list of event data
+  List events = [
+    [
+      'Dealing with Addiction',
+      'Join us and gain insights from experts, participate in workshops, and connect with a supportive community. Access resources, engage in Q&A sessions, and take the first step towards healing. \n\nWhether you\'re personally affected or supporting a loved one, find hope and guidance at our event. Reserve your spot today.',
+      '28 Dec, 2024',
+      '9AM to 1PM',
+      'Lumumba Grounds, New Kira Rd, Bukoto.',
+      AssetImage('assets/images/2148171701.jpg'),
+      '18:00'
+    ],
+    [
+      'Mental Health in Youth',
+      'Discover guidance from specialists, engage in workshops, and unite with a nurturing community. Access tools, join Q&A sessions, and embark on your journey to wellness. \n\nWhether you\'re impacted directly or supporting a friend, find solace and direction at our event. Secure your spot now.',
+      '18 Oct, 2024',
+      '9AM to 1PM',
+      'Serena Conference Center, Kampala.',
+      AssetImage('assets/images/2148171701.jpg'),
+      'Yesterday at 09:15'
+    ],
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,11 +61,28 @@ class _GroupEventsState extends State<GroupEvents> {
             ],
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
-          child: ListView(children: [
-            EventCard(),
-          ]),
+        body: ListView.builder(
+          itemCount: events.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+              child: Column(
+                // Changed from ListView to Column
+                children: [
+                  EventCard(
+                    eventTitle: events[index][0],
+                    eventDesc: events[index][1],
+                    eventDate: events[index][2],
+                    eventTime: events[index][3],
+                    eventLocation: events[index][4],
+                    eventImage: events[index][5],
+                    eventPostDate: events[index][6],
+                  ),
+                ],
+              ),
+            );
+          },
         ));
   }
 }
