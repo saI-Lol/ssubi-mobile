0675388387ded72d8a4084668aade34614a3d02b
Joshua Pirwoth <joshuapirwoth06@gmail.com>
2024-06-01 18:04:50 +0300

    Modified the code for this widget to use data support group data from the SupportGroup widget, instead of housing the same data itself

--- lib/tabs/all_groups.dart
+++ lib/tabs/all_groups.dart
@@ -4,44 +4,37 @@ import 'package:flutter/material.dart';
 import 'package:frontend/util/tab_bar_tile.dart';
 
 class AllGroups extends StatefulWidget {
-  const AllGroups({super.key});
+  // variables to hold data for support groups
+  final String groupName_;
+  final String createdBy_;
+  final int itemCount_;
+
+  const AllGroups({
+    required this.groupName_,
+    required this.createdBy_,
+    required this.itemCount_,
+  });
 
   @override
   State<AllGroups> createState() => _AllGroupsState();
 }
 
 class _AllGroupsState extends State<AllGroups> {
-// list of support groups
-  List supportGroups = [
-    ['Strong Minds Ug', 'Strong Minds Uganda'],
-    ['Mindverse Official Group', 'Mindverse'],
-    ['Serenity Circle', 'Mending Thoughts Ug'],
-    ['Calm Minds Collective', 'Peacful Mind'],
-    ['Peaceful Pathways', 'Kampala Serenity Center'],
-    ['Dealing with Anxiety', 'Strong Minds Uganda'],
-    ['Resilient Together', 'Phoenix Recovery Clinic'],
-    ['Tranquil Minds Network', 'Tranquility Mental Wellness Center'],
-    ['Stress Stops For Good', 'Mindverse'],
-    ['Strength in Unity', 'Unity MindCare Facility'],
-    ['Healing Hearts', 'Lighthouse Wellness Clinic'],
-    ['Thrive Together Support', 'Heart to Heart'],
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // const keyword removed
-        backgroundColor: Color(0xFFFCFEFF), // Bg color hexcode is #FCFEFF
-        body: ListView.builder(
-            itemCount: supportGroups.length,
-            itemBuilder: (context, index) {
-              return TabBarTile(
-                // algorithm that modifies avatar color absed on index
-                avatarColor:
-                    index % 2 == 0 ? Color(0xFF647AFF) : Color(0xFFB0BCFF),
-                groupName: supportGroups[index][0],
-                createdBy: supportGroups[index][1],
-              );
-            }));
+      backgroundColor: Color(0xFFFCFEFF), // Bg color hexcode is #FCFEFF
+      body: ListView.builder(
+        itemCount: widget.itemCount_, // Accessing widget properties
+        itemBuilder: (context, index) {
+          return TabBarTile(
+            // algorithm that modifies avatar color based on index
+            avatarColor: index % 2 == 0 ? Color(0xFF647AFF) : Color(0xFFB0BCFF),
+            groupName: widget.groupName_, // Accessing widget properties
+            createdBy: widget.createdBy_, // Accessing widget properties
+          );
+        },
+      ),
+    );
   }
 }
