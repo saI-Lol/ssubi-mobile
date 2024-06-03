3105812df7cfee169ebe0eab04e6b0fef3e14930
Nanziri Jovia <joviananziri813@gmail.com>
2024-06-03 14:00:57 -0700

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/event_reminder.dart';
 import 'package:flutter_application_1/pages/flash_screen.dart';
+import 'package:flutter_application_1/pages/home.dart';
+import 'package:flutter_application_1/pages/get_started.dart';
+import 'package:flutter_application_1/pages/crisis_hotlines.dart';
+import 'package:flutter_application_1/pages/daily_tips.dart';
+import 'package:flutter_application_1/pages/home.dart';
+
 
 
 
@@ -16,7 +23,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FlashScreen(),
+      home: Home(),
+      
+        
+      
+
+      
       
       
     );
