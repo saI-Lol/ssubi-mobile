2940b06666d29616b0a540bc2a19c1dfbfe4e378
Nanziri Jovia <joviananziri813@gmail.com>
2024-06-03 13:50:04 -0700

    added image, the arrow_back is a button

--- lib/pages/event_reminder.dart
+++ lib/pages/event_reminder.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -9,17 +9,31 @@ class EventReminder extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: Text("Crisis Hotlines"),
-        backgroundColor: Colors.blue,
+        title: Text("Reminder",
+        style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue[900],
         elevation: 0,
-        leading: Icon(Icons.arrow_back),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            // handle back button press
+
+          },
+        ),
       ),
 
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CircleAvatar(
+
+            Image.asset('lib/assets/6.png',
+            width: 200,
+            height: 200,
+            ),
+
+            /* CircleAvatar(
               radius: 40,
               backgroundColor: Colors.blue[100],
               child: Icon(
@@ -28,9 +42,9 @@ class EventReminder extends StatelessWidget {
                 color: Colors.blue,
 
               ),
-            ),
+            ),*/
 
-            SizedBox(height: 10),
+            SizedBox(height: 20),  // space between the image and text
             Text(
               'aynthing',
               textAlign: TextAlign.center,
