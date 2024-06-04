b6bd0735077f9366d87b1906fc9100a199d2f7a3
Joshua Pirwoth <joshuapirwoth06@gmail.com>
2024-06-04 15:48:19 +0300

    Withdrawn the gesture detector widget from the blog tile

--- lib/util/blog_tile.dart
+++ lib/util/blog_tile.dart
@@ -15,47 +15,43 @@ class BlogTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 16.0),
-      child: GestureDetector(
-        // method for navigating to blog details screen
-        onTap: () {},
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            color: Color(0xFFF6F7FF),
-          ),
-          child: Column(
-            children: [
-              // Blog title
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Text(
-                  blogTitle,
-                  style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                      color: Color(0xFF32313A)),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: Color(0xFFF6F7FF),
+        ),
+        child: Column(
+          children: [
+            // Blog title
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Text(
+                blogTitle,
+                style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF32313A)),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
               ),
+            ),
 
-              // Blog Author(s) and Date Published
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // Author
-                  Text(blogAuthor,
-                      style: const TextStyle(
-                          fontSize: 14, color: Color(0xFF647AFF))),
-                  // Date Published
-                  Text(blogDate,
-                      style: const TextStyle(
-                          fontSize: 14, color: Color(0xFF999999))),
-                ],
-              )
-            ],
-          ),
+            // Blog Author(s) and Date Published
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Author
+                Text(blogAuthor,
+                    style: const TextStyle(
+                        fontSize: 14, color: Color(0xFF647AFF))),
+                // Date Published
+                Text(blogDate,
+                    style: const TextStyle(
+                        fontSize: 14, color: Color(0xFF999999))),
+              ],
+            )
+          ],
         ),
       ),
     );
