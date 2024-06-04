188542a6d62a74b4c31a4347f20fa58be25a5da1
Nanziri Jovia <joviananziri813@gmail.com>
2024-06-04 12:30:04 -0700

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/event_reminder.dart';
-import 'package:flutter_application_1/pages/flash_screen.dart';
-import 'package:flutter_application_1/pages/home.dart';
+//import 'package:flutter_application_1/pages/event_reminder.dart';
+//import 'package:flutter_application_1/pages/flash_screen.dart';
+//import 'package:flutter_application_1/pages/home.dart';
 import 'package:flutter_application_1/pages/get_started.dart';
-import 'package:flutter_application_1/pages/crisis_hotlines.dart';
-import 'package:flutter_application_1/pages/daily_tips.dart';
-import 'package:flutter_application_1/pages/home.dart';
+//import 'package:flutter_application_1/pages/crisis_hotlines.dart';
+//import 'package:flutter_application_1/pages/daily_tips.dart';
+//import 'package:flutter_application_1/pages/home.dart';
 
 
 
