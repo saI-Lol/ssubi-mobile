5b78febfc7060cd5dbfd84624e01dccff7a3b5d1
Joshua Pirwoth <joshuapirwoth06@gmail.com>
2024-06-04 15:43:20 +0300

    Created a utility file for a tile that will render high level information of a blog

---
+++ lib/util/blog_tile.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class BlogTile extends StatelessWidget {
+  final String blogTitle;
+  final String blogAuthor;
+  final String blogDate;
+
+  const BlogTile(
+      {super.key,
+      required this.blogTitle,
+      required this.blogAuthor,
+      required this.blogDate});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0),
+      child: GestureDetector(
+        // method for navigating to blog details screen
+        onTap: () {},
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            color: Color(0xFFF6F7FF),
+          ),
+          child: Column(
+            children: [
+              // Blog title
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  blogTitle,
+                  style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF32313A)),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+
+              // Blog Author(s) and Date Published
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Author
+                  Text(blogAuthor,
+                      style: const TextStyle(
+                          fontSize: 14, color: Color(0xFF647AFF))),
+                  // Date Published
+                  Text(blogDate,
+                      style: const TextStyle(
+                          fontSize: 14, color: Color(0xFF999999))),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
