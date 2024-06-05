475165230ab6bdc5d20ae72e8f562e744a945135
Joshua Pirwoth <joshuapirwoth06@gmail.com>
2024-06-05 07:59:57 +0300

    Modified font weight for avatar content

--- lib/pages/support_groups.dart
+++ lib/pages/support_groups.dart
@@ -17,61 +17,97 @@ class _SupportGroupsState extends State<SupportGroups> {
     {
       'groupName': 'Strong Minds Ug',
       'createdBy': 'Strong Minds Uganda',
+      'description':
+          'Strong Minds Ug is a supportive community dedicated to promoting mental health awareness and providing resources for those in need. Join us to share your journey, gain insights from experts, and connect with others who understand.',
+      'dateCreated': '2023-05-15',
       'isJoined': false
     },
     {
       'groupName': 'Mindverse Official Group',
       'createdBy': 'Mindverse',
+      'description':
+          'Welcome to the official support group for Mindverse members! Here, you can engage in discussions about mental health, participate in events, and access exclusive resources to support your mental well-being.',
+      'dateCreated': '2023-07-22',
       'isJoined': false
     },
     {
       'groupName': 'Serenity Circle',
       'createdBy': 'Mending Thoughts Ug',
+      'description':
+          'Join the Serenity Circle to find peace and support on your mental health journey. Our community offers a safe space for sharing experiences, receiving encouragement, and accessing helpful resources.',
+      'dateCreated': '2023-04-10',
       'isJoined': false
     },
     {
       'groupName': 'Calm Minds Collective',
       'createdBy': 'Peacful Mind',
+      'description':
+          'The Calm Minds Collective is a group dedicated to promoting mindfulness and relaxation techniques to achieve inner peace. Join us for guided meditations, yoga sessions, and supportive discussions.',
+      'dateCreated': '2023-08-05',
       'isJoined': false
     },
     {
       'groupName': 'Peaceful Pathways',
       'createdBy': 'Kampala Serenity Center',
+      'description':
+          'Peaceful Pathways is your path to serenity and well-being. Our supportive community offers guidance, resources, and encouragement for navigating life\'s challenges. Join us to find peace and support.',
+      'dateCreated': '2023-06-18',
       'isJoined': false
     },
     {
       'groupName': 'Dealing with Anxiety',
       'createdBy': 'Strong Minds Uganda',
+      'description':
+          'Join our community for support and strategies for managing anxiety. Together, we\'ll explore coping mechanisms, share experiences, and provide encouragement to help each other through difficult times.',
+      'dateCreated': '2023-09-02',
       'isJoined': false
     },
     {
       'groupName': 'Resilient Together',
       'createdBy': 'Phoenix Recovery Clinic',
+      'description':
+          'Resilient Together is a community of resilience and strength. Join us as we support each other in overcoming challenges, building resilience, and finding hope for the future. Together, we are stronger.',
+      'dateCreated': '2023-08-21',
       'isJoined': false
     },
     {
       'groupName': 'Tranquil Minds Network',
       'createdBy': 'Tranquility Mental Wellness Center',
+      'description':
+          'Welcome to Tranquil Minds Network, a community dedicated to promoting mental wellness and providing support for those in need. Join us for resources, discussions, and a supportive network of peers.',
+      'dateCreated': '2023-10-09',
       'isJoined': false
     },
     {
       'groupName': 'Stress Stops For Good',
       'createdBy': 'Mindverse',
+      'description':
+          'Say goodbye to stress and hello to a happier, healthier you with Stress Stops For Good. Join our community for strategies, tips, and support in managing stress effectively and improving your well-being.',
+      'dateCreated': '2023-11-15',
       'isJoined': false
     },
     {
       'groupName': 'Strength in Unity',
       'createdBy': 'Unity MindCare Facility',
+      'description':
+          'Strength in Unity is a community where we come together to support each other on our mental health journeys. Join us as we share experiences, offer encouragement, and build resilience together.',
+      'dateCreated': '2023-10-30',
       'isJoined': false
     },
     {
       'groupName': 'Healing Hearts',
       'createdBy': 'Lighthouse Wellness Clinic',
+      'description':
+          'Healing Hearts is a supportive community for those seeking healing and emotional well-being. Join us for resources, discussions, and encouragement as we journey towards healing together.',
+      'dateCreated': '2023-12-07',
       'isJoined': false
     },
     {
       'groupName': 'Thrive Together Support',
       'createdBy': 'Heart to Heart',
+      'description':
+          'Thrive Together Support is a community dedicated to empowering individuals to thrive in their mental health journey. Join us for support, resources, and encouragement as we thrive together.',
+      'dateCreated': '2024-01-12',
       'isJoined': false
     },
   ];
@@ -198,7 +234,7 @@ class AllGroups extends StatelessWidget {
                             style: const TextStyle(
                                 fontSize: 28.0,
                                 color: Color(0xFFFCFEFF),
-                                fontWeight: FontWeight.w200),
+                                fontWeight: FontWeight.w300),
                           ),
                         ),
 
